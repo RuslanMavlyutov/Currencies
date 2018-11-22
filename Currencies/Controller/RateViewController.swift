50dc9f7d3498a24448477a2d94aa5defba1139a2
RuslanMavlyutov <piercerum@gmail.com>
2018-11-22 17:54:06 +0400

    Added timer to RateViewController to auto refresh currencies

--- Currencies/Controller/RateViewController.swift
+++ Currencies/Controller/RateViewController.swift
@@ -13,6 +13,7 @@ final class RateViewController: UIViewController {
 
     @IBOutlet weak var priceUsdLabel: UILabel!
     @IBOutlet weak var priceBTCLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
 
     @IBAction func updateLabel(_ sender: UIButton) {
         requestRate()
@@ -20,23 +21,29 @@ final class RateViewController: UIViewController {
 
     static let CBR_LINK = "https://www.cbr-xml-daily.ru/daily_json.js"
     static let CRYPTO_LINK = "https://api.coindesk.com/v1/bpi/currentprice.json"
+    static let format = "yyyy-MM-dd HH:mm:ss"
     static let text = "..."
 
     private let exchangeRates = ExchangeRates()
+    private var updateTimer: Timer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         priceUsdLabel.text = RateViewController.text
         priceBTCLabel.text = RateViewController.text
+        timeLabel.text = ""
 
         exchangeRates.rateObject(urlString: RateViewController.CBR_LINK) {
             response in
             print(response)
             self.priceUsdLabel.text = "$1 = \(response) руб."
         }
+
+        updateTimer = Timer.scheduledTimer(timeInterval: 10, target: self, selector: #selector(requestRate),
+                                     userInfo: nil, repeats: true)
     }
 
-    func requestRate () {
+    @objc func requestRate () {
         exchangeRates.rateObject(urlString: RateViewController.CBR_LINK) {
             response in
             print(response)
@@ -46,6 +53,10 @@ final class RateViewController: UIViewController {
             response in
             print(response)
             self.priceBTCLabel.text = "1 BTC = $\(response)"
+
+            let formatter = DateFormatter()
+            formatter.dateFormat = RateViewController.format
+            self.timeLabel.text = formatter.string(from: Date())
         }
     }
 }
